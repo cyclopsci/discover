71f7d602d340310392175e85e158bfc9d604cc33
nextrevision <notarobot@nextrevision.net>
2015-05-14 17:09:05 -0400

    Removing main package from gitignore

---
+++ discover/discover.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"flag"
+	"encoding/json"
+	"github.com/cyclopsci/discover"
+)
+
+var (
+	root = flag.String("directory", ".", "directory to discover")
+)
+
+func main() {
+	flag.Parse()
+	results := discover.Run(*root)
+	json_results, _ := json.MarshalIndent(results, "", "\t")
+	fmt.Println(string(json_results))
+}
